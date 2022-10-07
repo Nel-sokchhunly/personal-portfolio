995ecbf4623d3a05b63eb53f9cc3008bc4a90d79
Sokchhunly-Quad <sokchhunly.nel@quad.co.jp>
2022-10-08 01:01:35 +0700

    finished project page

--- data/projects.ts
+++ data/projects.ts
@@ -2,6 +2,7 @@ export interface projectsType {
   poster: string;
   title: string;
   description: string;
+  technologies: string[];
 }
 
 export const projects: projectsType[] = [
@@ -9,18 +10,24 @@ export const projects: projectsType[] = [
     poster:
       'https://firebasestorage.googleapis.com/v0/b/personal-portfolio-7cbca.appspot.com/o/jart%20jaeng.png?alt=media&token=bc132790-4d27-468f-ad75-74d423c34ddb',
     title: 'ចាត់ចែង - Jart Jaeng',
-    description: 'this is project 1 description'
+    description:
+      "I've joined Flutter training bootcamp and after a month and a half this is the result. I've participated in a small group and built this flutter application.\n\nIn this training program, We also came first as the best application that stands out the most by vote.",
+    technologies: ['Flutter', 'Dart', 'Firebase', 'Figma']
   },
   {
     poster:
       'https://firebasestorage.googleapis.com/v0/b/personal-portfolio-7cbca.appspot.com/o/penhwonders-08102022-0001.jpg?alt=media&token=74d3bb51-9bcb-4955-9f9f-7434b2c8b781',
     title: 'Penhwonder',
-    description: 'this is project 2 description'
+    description:
+      'A web application + Automation built by Cambodian college students to help your life easier in finding and promoting wonderful events in Phnom Penh! The events are automatically updated from a list of registered Facebook pages once everyday at 12:30am by our automated events-creating system.',
+    technologies: ['VueJS', 'Vuex', 'Vuetify']
   },
   {
     poster:
       'https://firebasestorage.googleapis.com/v0/b/personal-portfolio-7cbca.appspot.com/o/odoo%20image.png?alt=media&token=f5a4b7b3-c8d2-461b-9818-d7fd3c315b34',
     title: 'Odoo application',
-    description: 'this is project 3 description'
+    description:
+      'Participate in building an odoo enterprise application for A2A internal use that is more that 500 users. The project was a huge success.',
+    technologies: ['Odoo', 'Python', 'XML', 'Postgres']
   }
 ];
--- pages/project.tsx
+++ pages/project.tsx
@@ -3,6 +3,7 @@ import MainLayout from '../layouts/MainLayout';
 import { motion } from 'framer-motion';
 import { projects, projectsType } from '../data/projects';
 import Image from 'next/image';
+import { spawn } from 'child_process';
 
 export default function Project() {
   return (
@@ -18,12 +19,13 @@ function ProjectPage({ projects, amount }) {
   const updateActivePage = (index) => setActivePage(index);
 
   return (
-    <div className='overflow-hidden h-full flex flex-col'>
-      <div className='h-full'>
-        <ProjectDetailPage index={activePage} />
-      </div>
+    <div className='overflow-hidden h-full flex-grow flex flex-col relative'>
+      <ProjectDetailPage index={activePage} />
       {/* projects bar */}
-      <motion.div className='w-full h-fit bg-accent2 flex' layout>
+      <motion.div
+        className='w-full h-fit bg-accent2 flex sticky bottom-0 left-0'
+        layout
+      >
         {projects.map((project: projectsType, index) => (
           <ProjectNavBtn
             index={index}
@@ -40,15 +42,30 @@ function ProjectPage({ projects, amount }) {
 const ProjectDetailPage = ({ index }) => {
   const project = projects[index];
   return (
-    <div className='p-4'>
+    <div className='p-4 flex flex-col sm:flex-row h-full'>
       <div className='flex justify-center sm:justify-start'>
-        <div className='h-72 w-full md:w-96 border-2 border-secondary flex flex-col hover:shadow-sm hover:shadow-secondary transition duration-200'>
+        <div className='h-72 w-full sm:w-96 border-2 border-secondary flex flex-col hover:shadow-sm hover:shadow-secondary transition duration-200'>
           <div className='relative flex-1 w-full flex items-center justify-center bg-opacity-30 font-medium whitespace-pre-wrap'>
             <Image src={project.poster} layout='fill' objectFit='cover' />
           </div>
-          <div className='text-center border-t-2 border-black py-4 bg-opacity-50 hover:bg-primary focus:bg-primary'>
-            Nel Sokchhunly
+          <div className='font-semibold text-center border-t-2 border-black py-4 bg-opacity-50 hover:bg-primary focus:bg-primary'>
+            {project.title}
           </div>
+        </div>
+      </div>
+      <div className='w-full xl:w-3/5 sm:pl-4 pt-4 sm:pt-0'>
+        <div className='text-xl hidden sm:block font-semibold'>
+          {project.title}
+        </div>
+        <div>{project.description}</div>
+        <br />
+        <div>
+          Technologies:{' '}
+          {project.technologies.map((tech) => (
+            <span className='m-2 p-1 px-2 border-2 border-accent2 rounded-full'>
+              {tech}
+            </span>
+          ))}
         </div>
       </div>
     </div>
@@ -57,7 +74,8 @@ const ProjectDetailPage = ({ index }) => {
 
 const ProjectNavBtn = ({ index, activePage, updateActivePage }) => {
   // scenario
-  let activeButton = 'bg-primary text-black flex-grow text-center';
+  let activeButton =
+    'bg-primary text-black flex-grow sm:flex-grow-0 text-center';
   let inActiveButton = '';
   // btn 1 (activePage 0) => btn1:primary, btn2:accent-1, btn3:accent-2
   if (activePage === 0) {
@@ -77,7 +95,7 @@ const ProjectNavBtn = ({ index, activePage, updateActivePage }) => {
 
   return (
     <motion.div
-      className={`px-8 py-4  ${
+      className={`px-8 py-4 cursor-pointer  ${
         activePage === index ? activeButton : inActiveButton
       }`}
       onClick={() => updateActivePage(index)}
