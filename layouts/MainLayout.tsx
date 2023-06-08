653db13e841634ee792197c11824314ec4eabded
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2023-06-08 21:58:08 +0700

    fix: layout

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ type BackNavbarProps = {
 export default function MainLayout({ backUrl, children }: BackNavbarProps) {
   return (
     <div className='overflow-hidden h-screen  bg-primary flex flex-col relative'>
-      <div className='h-full flex-grow overflow-y-scroll mb-16'>{children}</div>
+      <div className='h-full flex-grow overflow-y-scroll pb-16'>{children}</div>
       <Link href={backUrl ? backUrl : '/'}>
         <div className='w-full bg-primary cursor-pointer h-16 flex justify-center items-center border-t-2 border-secondary px-4 fixed bottom-0 right-0'>
           <div className='font-semibold text-lg text-secondary'>Back</div>
