2b5beab58e520e546f69b6adfc972dfbc80f0335
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2023-07-31 11:44:07 +0700

    fix: about me

---
+++ components/socials.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+
+type Social = {
+  name: string;
+  icon: string;
+  link: string;
+};
+
+const socialList: Social[] = [
+  {
+    name: 'Resume',
+    icon: '/assets/cv.png',
+    link: 'https://drive.google.com/file/d/1XU4Ms1qe-N8eXH2_i4SFyDFdiF6Z1hWc/view?usp=sharing'
+  },
+  {
+    name: 'Github',
+    icon: '/assets/github.png',
+    link: 'https://github.com/Nel-sokchhunly'
+  },
+  {
+    name: 'LinkedIn',
+    icon: '/assets/linkedin.png',
+    link: 'https://www.linkedin.com/in/sokchhunly-nel-2926341b9/'
+  },
+  {
+    name: 'sokchhunlynel@gmail.com',
+    icon: '/assets/envelope.png',
+    link: 'mailto:sokchhunlynel@gmail.com'
+  }
+];
+
+export default function Socials() {
+  return (
+    <div className='mt-4'>
+      <div className='w-full flex flex-row flex-wrap gap-4 mt-2 mb-10'>
+        {socialList.map((social, index) => (
+          <SocialAction key={index} {...social} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const SocialAction = ({ icon, name, link }: Social) => {
+  return (
+    <a href={link} target='_blank' className='cursor-pointer w-fit'>
+      <div
+        className='
+              flex items-start space-x-2 p-2 px-3 h-full w-full
+              border-2 border-dashed border-secondary 
+              hover:translate-x-1 hover:-translate-y-1
+              hover:shadow-[-4px_4px_0_black]
+              transition-all duration-300 
+              rounded-lg hover:rounded-2xl
+            '
+      >
+        <Image src={icon} alt='linkedin' width='24' height='24' />
+        <h1>{name}</h1>
+      </div>
+    </a>
+  );
+};
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,13 @@
 import MainLayout from '../layouts/MainLayout';
 import Image from 'next/image';
 
-const resumeLink = process.env.RESUME_URL;
+import { Lora } from 'next/font/google';
+import Socials from '../components/socials';
+
+const lora = Lora({
+  display: 'swap',
+  subsets: ['latin']
+});
 
 export default function About() {
   return (
@@ -26,125 +32,36 @@ export default function About() {
                   className='object-cover'
                 />
               </div>
-              <div className='text-center border-t-2 border-stone-700 py-4 bg-opacity-50'>
-                Nel Sokchhunly
-              </div>
             </div>
 
             <div
               className='
-              h-full w-full z-0 bg-black bg-opacity-80 absolute left-0 top-0
-              translate-y-1 -translate-x-1
-              transition duration-300 rounded-sm
-            '
+                h-full w-full z-0 bg-black bg-opacity-80 absolute left-0 top-0
+                translate-y-1 -translate-x-1
+                transition duration-300 rounded-sm
+              '
             ></div>
           </div>
         </div>
         <div>
-          <p className='flex-grow sm:flex-grow-0 sm:w-3/4 '>
-            Hello, My name is{' '}
-            <span className='font-medium'>Nel Sokchhunly</span>
-            <br />
-            <br />
-            I am a sponsorship student majoring in a Software Engineering major
-            at Kirirom Institute of Technology, locating here on top of Kirirom
-            mountain.
-            <br />
-            <br />I am specializing in Frontend web development. I am proficient
-            in working with <b>NextJS</b>, <b>ReactJS</b>, <b>TailwindCSS</b>.
-            Aside from frontend web development, I also have worked on various
-            development aspect such as building mobile application using{' '}
-            <b>Flutter</b>, building an internal enterprise application using{' '}
-            <b>Odoo</b> technology. I also have experience working with{' '}
-            <b>Figma</b>.
-          </p>
-
-          <div className='w-full flex flex-col space-y-4 mt-4 mb-10'>
-            <a
-              href='https://drive.google.com/file/d/1XU4Ms1qe-N8eXH2_i4SFyDFdiF6Z1hWc/view?usp=sharing'
-              target='_blank'
-              className='cursor-pointer w-fit'
-            >
-              <div className='flex items-start space-x-2 '>
-                <Image
-                  src='/assets/cv.png'
-                  alt='resume'
-                  width='24'
-                  height='24'
-                />
-                <span>View my resume</span>
-                <Image
-                  src='/assets/open-link.png'
-                  alt='open-link'
-                  width='14'
-                  height='14'
-                />
-              </div>
-            </a>
-            <a
-              href='mailto:sokchhunlynel@example.com'
-              target='_blank'
-              className='cursor-pointer w-fit'
-            >
-              <div className='flex items-start space-x-2'>
-                <Image
-                  src='/assets/envelope.png'
-                  alt='mail'
-                  width='24'
-                  height='24'
-                />
-                <span>sokchhunlynel@gmail.com</span>
-                <Image
-                  src='/assets/open-link.png'
-                  alt='open-link'
-                  width='14'
-                  height='14'
-                />
-              </div>
-            </a>
-            <a
-              href='https://github.com/Nel-sokchhunly'
-              target='_blank'
-              className='cursor-pointer w-fit'
-            >
-              <div className='flex items-start space-x-2'>
-                <Image
-                  src='/assets/github.png'
-                  alt='github'
-                  width='24'
-                  height='24'
-                />
-                <span>Nel-sokchhunly</span>
-                <Image
-                  src='/assets/open-link.png'
-                  alt='open-link'
-                  width='14'
-                  height='14'
-                />
-              </div>
-            </a>
-            <a
-              href='https://www.linkedin.com/in/sokchhunly-nel-2926341b9/'
-              target='_blank'
-              className='cursor-pointer w-fit'
-            >
-              <div className='flex items-start space-x-2'>
-                <Image
-                  src='/assets/linkedin.png'
-                  alt='linkedin'
-                  width='24'
-                  height='24'
-                />
-                <span>Sokchhunly Nel</span>
-                <Image
-                  src='/assets/open-link.png'
-                  alt='open-link'
-                  width='14'
-                  height='14'
-                />
-              </div>
-            </a>
+          <div className='flex-grow sm:flex-grow-0 lg:w-3/4'>
+            <span>Hello there, My name is</span>
+            <div className='font-semibold text-2xl my-4 ' style={lora.style}>
+              Nel Sokchhunly
+            </div>
+            <p>
+              I am a <b>Software Engineer</b> specializing in{' '}
+              <b>Frontend web development</b>. I am proficient in working with{' '}
+              <b>NextJS</b>, <b>ReactJS</b>, <b>TailwindCSS</b>. Aside from
+              frontend web development, I also have worked on various
+              development aspect such as building mobile application using{' '}
+              <b>Flutter</b>, building an internal enterprise application using{' '}
+              <b>Odoo</b> technology. I also have experience working with{' '}
+              <b>Figma</b>.
+            </p>
           </div>
+
+          <Socials />
         </div>
       </div>
     </MainLayout>
